5091db51a2ad19c9d752126c3c823460bf574102
MonajiLee <lee.smonica@gmail.com>
2018-10-11 01:26:50 +0000

    Created AJAX GET request of tweets from server

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,70 +1,32 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Fake data taken from tweets.json
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-];
+$(() => {
 
 
-// Task 3
+// Task 4 --- create AJAX GET request from the server
+function loadTweets(tweetDatabase) {
+    $.ajax({
+        url:'/tweets',
+        method:'GET',
+        success: function(result){
+            renderTweets(result);
+        }
+    });
+}
+
+
+
+// Task 3 --- creates AJAX POST request to the server ---
 $('form').on('submit', function(event) {
     event.preventDefault();
     let tweetInput = $(event.target).serialize();
-    $.ajax("/tweets/", {method: 'POST', data: tweetInput}).then(() => {
-        console.log("This is working");
+    $.ajax("/tweets/", {
+        method: 'POST', 
+        data: tweetInput 
+    }).then(() => {
     });
 });
 
 // Task 2 ----- renders tweets dynamically in relation to Task 1 -----
 function renderTweets(tweetArr) {
-    // loops through tweets (array of tweet objects)
     $.each(tweetArr, function(index, value) {
         $(createTweetElement(value)).appendTo("#tweets-container");
     })
@@ -94,4 +56,5 @@ function createTweetElement(tweetObj) {
         .append($date);
 }
 
-renderTweets(data);
+loadTweets();
+});
